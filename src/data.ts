66f5cd14720cdae90e97d95ac459a1e5fa3fc0e9
osawa-koki <github@osawa.dev>
2022-12-24 14:35:12 +0900

    小さな修正。

--- src/data.ts
+++ src/data.ts
@@ -1,12 +1,14 @@
 
 const chart_bar_data = {
   labels: ['C#', 'TypeScript', 'Python', 'Haskell', 'Ruby', 'Kotlin'],
-  datasets: [{
-    label: 'programming languages',
-    data: [12, 19, 3, 5, 2, 23],
-    borderWidth: 1,
-    borderColor: "blue",
-  }],
+  datasets: [
+    {
+      label: 'programming languages',
+      data: [12, 19, 3, 5, 2, 23],
+      borderWidth: 1,
+      borderColor: "blue",
+    },
+  ],
 };
 
 export { chart_bar_data };
